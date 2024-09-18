f72a1e4ec9952a82bdd1f216269ff69cd57ce9e6
kilisei <seitzkili+Github@gmail.com>
2024-09-18 09:41:22 +0200

    init

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+
+const count = ref(0)
+const double = computed(() => count.value * 2)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  {{ count }}
+  {{ double }}
+  <button @click="count++">
+    Click
+  </button>
+</template>
