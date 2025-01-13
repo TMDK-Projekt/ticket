3c84ff5efcbcb98cc01f6833bc49324628f29295
Timo Holzammer <ti.holzammer@gmail.com>
2025-01-13 10:23:12 +0100

    feature setup auth pages

---
+++ frontend/app/pages/auth/signIn.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+  <UiCard>
+      <UiCardHeader class="text-center">
+        <UiCardTitle>LogIn</UiCardTitle>
+      </UiCardHeader>
+
+    <UiCardContent>
+
+      <UiLabel>Name</UiLabel>
+      <UiInput></UiInput>
+
+      <UiLabel>Passwort</UiLabel>
+      <UiInput></UiInput>
+    </UiCardContent>
+
+    <UiCardFooter>
+      <div class="w-full flex gap-4 justify-evenly">
+        <UiButton as-child variant="secondary">
+          <NuxtLink to="/auth/signUp"> Registrieren </NuxtLink>
+        </UiButton>
+        <UiButton>Anmelden</UiButton>
+      </div>
+    </UiCardFooter>
+  </UiCard>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/app/pages/auth/signUp.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+  <UiCard>
+    <UiCardHeader class="text-center">
+      <UiCardTitle>SignUp</UiCardTitle>
+    </UiCardHeader>
+    <UiCardContent>
+
+      <UiLabel>Name</UiLabel>
+      <UiInput></UiInput>
+
+      <UiLabel>Email</UiLabel>
+      <UiInput type="email"></UiInput>
+
+      <UiLabel>Passwort</UiLabel>
+      <UiInput></UiInput>
+    </UiCardContent>
+
+    <UiCardFooter>
+      <div class="w-full flex gap-4 justify-evenly">
+        <UiButton as-child variant="secondary">
+          <NuxtLink to="/auth/signIn"> Login </NuxtLink>
+        </UiButton>
+        <UiButton>Registrieren</UiButton>
+      </div>
+    </UiCardFooter>
+  </UiCard>
+
+</template>
+
+<style scoped>
+
+</style>
