e5d531f62231597320868fe91f9bac96b12f0350
Timo Holzammer <ti.holzammer@gmail.com>
2025-01-13 13:58:32 +0100

    feature updated auth pages

--- frontend/app/pages/auth/signIn.vue
+++ frontend/app/pages/auth/signIn.vue
@@ -1,29 +1,39 @@
 <script setup lang="ts">
+function signIn() {
+  const data = $fetch("http://localhost:8484/user", {
+    method: "post",
+    body: values.value
+  })
+}
 
+const values = ref({
+  email: null,
+  password: null
+})
 </script>
 
 <template>
 
   <UiCard>
       <UiCardHeader class="text-center">
-        <UiCardTitle>LogIn</UiCardTitle>
+        <UiCardTitle>Login</UiCardTitle>
       </UiCardHeader>
 
     <UiCardContent>
 
-      <UiLabel>Name</UiLabel>
-      <UiInput></UiInput>
+      <UiLabel>E-Mail</UiLabel>
+      <UiInput v-model="values.email" type="email"/>
 
       <UiLabel>Passwort</UiLabel>
-      <UiInput></UiInput>
+      <UiInput v-model="values.password" type="password"/>
     </UiCardContent>
 
     <UiCardFooter>
-      <div class="w-full flex gap-4 justify-evenly">
+      <div class="w-full flex gap-4 justify-end">
         <UiButton as-child variant="secondary">
           <NuxtLink to="/auth/signUp"> Registrieren </NuxtLink>
         </UiButton>
-        <UiButton>Anmelden</UiButton>
+        <UiButton @click="signIn">Anmelden</UiButton>
       </div>
     </UiCardFooter>
   </UiCard>
--- frontend/app/pages/auth/signUp.vue
+++ frontend/app/pages/auth/signUp.vue
@@ -1,33 +1,50 @@
 <script setup lang="ts">
+function signUp() {
+  const data = $fetch("http://localhost:8484/user", {
+    method: "post",
+    body: values.value
+  })
+}
 
+const values = ref({
+  firstName: null,
+  lastName: null,
+  email: null,
+  password: null
+})
 </script>
 
 <template>
 
   <UiCard>
     <UiCardHeader class="text-center">
-      <UiCardTitle>SignUp</UiCardTitle>
+      <UiCardTitle>Registrieren</UiCardTitle>
     </UiCardHeader>
-    <UiCardContent>
 
-      <UiLabel>Name</UiLabel>
-      <UiInput></UiInput>
+      <UiCardContent>
 
-      <UiLabel>Email</UiLabel>
-      <UiInput type="email"></UiInput>
+        <UiLabel>Vorname</UiLabel>
+        <UiInput v-model="values.firstName"/>
 
-      <UiLabel>Passwort</UiLabel>
-      <UiInput></UiInput>
-    </UiCardContent>
+        <UiLabel>Nachname</UiLabel>
+        <UiInput v-model="values.lastName"/>
 
-    <UiCardFooter>
-      <div class="w-full flex gap-4 justify-evenly">
-        <UiButton as-child variant="secondary">
-          <NuxtLink to="/auth/signIn"> Login </NuxtLink>
-        </UiButton>
-        <UiButton>Registrieren</UiButton>
-      </div>
-    </UiCardFooter>
+        <UiLabel>Email</UiLabel>
+        <UiInput v-model="values.email" type="email"/>
+
+        <UiLabel>Passwort</UiLabel>
+        <UiInput v-model="values.password" type="password"/>
+      </UiCardContent>
+
+      <UiCardFooter>
+        <div class="flex gap-4 justify-end">
+          <UiButton as-child variant="secondary">
+            <NuxtLink to="/auth/signIn"> Login </NuxtLink>
+          </UiButton>
+          <UiButton @click="signUp">Registrieren</UiButton>
+        </div>
+      </UiCardFooter>
+
   </UiCard>
 
 </template>
